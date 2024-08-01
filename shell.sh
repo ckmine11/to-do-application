407ca8fb5a2a70b51c6c7643aca22429545ed90f
ckmine11 <167895891+ckmine11@users.noreply.github.com>
2024-08-01 16:19:45 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -3,7 +3,7 @@ cd /var/lib/jenkins/workspace/to-do-application/
 #sudo su - jenkins -s/bin/bash
 #docker logout   harbor.cluster.com/sample-project
 docker login -u admin --password root harbor.cluster.com
-sudo  docker image build -t  $JOB_NAME:v1.$BUILD_ID .
+docker image build -t  $JOB_NAME:v1.$BUILD_ID .
 docker image tag $JOB_NAME:v1.$BUILD_ID harbor.cluster.com/to-do-application/$JOB_NAME:v1.$BUILD_ID
 docker image tag $JOB_NAME:v1.$BUILD_ID harbor.cluster.com/to-do-application/$JOB_NAME:latest
 docker image push harbor.cluster.com/to-do-application/$JOB_NAME:v1.$BUILD_ID
