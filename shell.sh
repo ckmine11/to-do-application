6cd97afe18d8e4a6bf22d11c3a37ef2f2d08dc7d
ckmine11 <167895891+ckmine11@users.noreply.github.com>
2024-08-01 16:04:11 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -2,10 +2,10 @@
 cd /var/lib/jenkins/workspace/to-do-application/
 #sudo su - jenkins -s/bin/bash
 #docker logout   harbor.cluster.com/sample-project
-sudo docker login -u admin --password root harbor.cluster.com
-  sudo docker image build -t  $JOB_NAME:v1.$BUILD_ID .
-sudo docker image tag $JOB_NAME:v1.$BUILD_ID harbor.cluster.com/to-do-application/$JOB_NAME:v1.$BUILD_ID
-sudo docker image tag $JOB_NAME:v1.$BUILD_ID harbor.cluster.com/to-do-application/$JOB_NAME:latest
-sudo docker image push harbor.cluster.com/to-do-application/$JOB_NAME:v1.$BUILD_ID
-sudo docker  push harbor.cluster.com/to-do-application/$JOB_NAME:latest
+docker login -u admin --password root harbor.cluster.com
+  docker image build -t  $JOB_NAME:v1.$BUILD_ID .
+docker image tag $JOB_NAME:v1.$BUILD_ID harbor.cluster.com/to-do-application/$JOB_NAME:v1.$BUILD_ID
+docker image tag $JOB_NAME:v1.$BUILD_ID harbor.cluster.com/to-do-application/$JOB_NAME:latest
+docker image push harbor.cluster.com/to-do-application/$JOB_NAME:v1.$BUILD_ID
+docker  push harbor.cluster.com/to-do-application/$JOB_NAME:latest
  #docker image rmi $JOB_NAME:v1.$BUILD_ID  harbor.cluster.com/sample-project/$JOB_NAME:v1.$BUILD_ID   harbor.cluster.com/sample-project/$JOB_NAME:latest 
